2e03fa5621aca7c318316064f07b81c124d5bacf
Paweł Czwarno <pawel.czwarno@gmail.com>
2024-02-11 15:38:30 +0100

    added renderTasks and changed flex to grid in list

--- JS/script.js
+++ JS/script.js
@@ -38,20 +38,27 @@
         })
     };
 
-    const render = () => {
+    const renderTasks = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-            <li class="list__element">
-                <button class="list__button list__button--done js-done">${task.done ? "✓" : ""}</button>
-                <div class="child-2 ${task.done ? "list__element--done" : ""}">${task.content}</div>
-                <button class="list__button list__button--remove js-remove"><img class="list__button--image" src="./images/trash.png" alt="trash_icon"></button>
-            </li>
-            `;
+        <li class="list__element">
+            <button class="list__button list__button--done js-done">${task.done ? "✓" : ""}</button>
+            <div class="${task.done ? "list__element--done" : ""}">${task.content}</div>
+            <button class="list__button list__button--remove js-remove"><img class="list__button--image" src="./images/trash.png" alt="trash_icon"></button>
+        </li>
+        `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const renderButtons = () => { };
+
+    const render = () => {
+        renderTasks();
+        renderButtons();
 
         bindEvents();
     };
@@ -59,13 +66,13 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const newTaskInput = document.querySelector(".js-newTask")
+        const newTaskInput = document.querySelector(".js-newTask");
         const newTaskContent = newTaskInput.value.trim();
 
         if (newTaskContent !== "") {
             addNewTask(newTaskContent);
             newTaskInput.value = "";
-        }
+        };
 
         newTaskInput.focus();
     };
